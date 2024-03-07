9f02e6b90adcf15a353c78f895f13a032e5b582d
shriyans jindal <shriyansjindal40517@gmail.com>
2024-03-07 16:28:41 +0530

    joi validation improvement done

--- Backend/validator.js
+++ Backend/validator.js
@@ -4,7 +4,7 @@ const validator =(schema)=>(payload)=> schema.validate(payload,{abortEarly:false
 
 const Schema = Joi.object({
     name:Joi.string().required(),
-    year:Joi.number(),
+    year:Joi.number().integer().min(1200).max(2024),
     description:Joi.string().required(),
     feature:Joi.string().required(),
     image:Joi.string().required()
