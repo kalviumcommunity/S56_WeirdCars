038a43bcf59f7289f08b7844b16cb03633da884d
shriyans jindal <shriyansjindal40517@gmail.com>
2024-03-08 13:00:18 +0530

    login logout cookies added

---
+++ WeirdCars/src/Page/LoginPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../Components/Navbar'
+import Login from '../Components/Login'
+
+const LoginPage = () => {
+  return (
+    <div>
+        <Navbar/>
+        <Login/>
+    </div>
+  )
+}
+
+export default LoginPage
