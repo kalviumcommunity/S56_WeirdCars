59b608d140fd0b9264eff9221ee38ed52c64a6bd
shriyans jindal <shriyansjindal40517@gmail.com>
2024-03-12 11:59:39 +0530

    token added in cookie

--- WeirdCars/src/Components/Login.jsx
+++ WeirdCars/src/Components/Login.jsx
@@ -3,15 +3,17 @@ import "./Login.css"
 import { useNavigate } from 'react-router-dom'
 import axios from "axios"
 const Login = () => {
-    const [username,setName] = useState()
-    const [password,setPassword] = useState()
+    const [username,setName] = useState('')
+    const [password,setPassword] = useState('')
     const navigate = useNavigate()
     let handleSubmit=(e)=>{
         e.preventDefault()
         document.cookie = `username=${username};expires=` +new Date(2030,0,1).toUTCString
         axios.post("https://weirdcars.onrender.com/auth",{username,password})
         .then((res)=>{
-            console.log(res)
+            // console.log(res)
+            document.cookie = `token=${res.data}; expires=` +new Date(2030,0,1).toUTCString
+
         })
         .catch((err)=>console.log(err))
         navigate("/")
